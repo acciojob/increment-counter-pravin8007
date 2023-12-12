6e04bf01da1d1b0b9d5a5186775f2649b7f69a08
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-12 11:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,20 +3,15 @@ const counterElement = document.getElementById('counter');
     const incrementButton = document.getElementById('incrementBtn');
 
 	counterElement.textContent = 0;
-    // Set the initial value of the counter
     let counterValue = 0;
 
-    // Function to handle the button click
+
     function handleIncrement() {
-      // Display an alert with the current un-incremented value
+		
       alert(counterValue);
-
-      // Increment the counter value
       counterValue++;
-
-      // Update the counter element with the new value
       counterElement.textContent = counterValue;
     }
 
-    // Attach the handleIncrement function to the button click event
+
     incrementButton.addEventListener('click', handleIncrement);
